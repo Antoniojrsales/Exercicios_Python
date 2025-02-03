7356c87e1f7d23e47cb1dfc3dc86593b0ac403be
Antoniojrsales <Antoniogomes.junio@gmail.com>
2025-02-02 21:51:14 -0300

    atualizando

---
+++ Exercicio19.py
@@ -0,0 +1,7 @@
+'''2. Manipulação de Strings
+Escreva um programa que:
+
+Solicite ao usuário que insira uma frase.
+Conte e exiba quantas palavras a frase possui.
+Verifique se a palavra "Python" está na frase e exiba uma mensagem informando.
+Inverta e exiba a frase digitada.'''
--- tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-A programação é incrível!
